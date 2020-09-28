215b98b30df95d9acc2736b28f07780d6500ea24
takumi <takumik9823@gmail.com>
2020-09-29 01:48:08 +0900

    仕様不備を修正

--- db/db-challenge3/db-010/db-010.sql
+++ db/db-challenge3/db-010/db-010.sql
@@ -1,20 +1,23 @@
-SELECT users.name AS 'ユーザー名', table1.tmp AS '投稿日時', chatrooms.name AS 'チャットルーム名'
+SELECT users.name AS 'ユーザー名', posts.create_datetime AS '投稿日時', chatrooms.name AS 'チャットルーム名'
 FROM(
-    SELECT create_user_id,room_id,tmp
+    SELECT MAX(id) AS p_max, p2.tmp2 AS c_max
     FROM(
         SELECT
         MAX(create_datetime) AS tmp,room_id tmp2
         FROM posts
         WHERE is_deleted=false
         GROUP BY room_id
-        order by room_id DESC
+        ORDER BY room_id DESC
     ) AS p2
     JOIN posts
     ON p2.tmp=posts.create_datetime AND p2.tmp2=posts.room_id
+    GROUP BY p2.tmp2
 ) AS table1
-JOIN users
-ON table1.create_user_id=users.id
+JOIN posts
+ON posts.id=table1.p_max
 JOIN chatrooms
-ON table1.room_id=chatrooms.id
-WHERE users.is_deleted=false AND chatrooms.is_deleted=false
-ORDER BY chatrooms.id ASC
+ON chatrooms.id=table1.c_max
+JOIN users
+ON users.id=posts.create_user_id
+WHERE users.is_deleted=false AND posts.is_deleted=false
+ORDER BY table1.p_max ASC;
