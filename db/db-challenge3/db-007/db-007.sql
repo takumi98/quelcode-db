120d5cc88d3733bc090bdb70570ce09772dabf31
takumi <takumik9823@gmail.com>
2020-09-29 15:56:02 +0900

    007 LIKEの条件を変更

--- db/db-challenge3/db-007/db-007.sql
+++ db/db-challenge3/db-007/db-007.sql
@@ -3,5 +3,5 @@ CASE is_file_sendable WHEN 0 THEN '禁止' WHEN 1 THEN '許可' END AS is_file_s
 is_direct_chat,is_deleted,create_user_id,create_datetime,update_user_id,update_datetime
 FROM chatrooms
 WHERE is_deleted=0 
-AND RIGHT(overview,9) LIKE '%ダイレクトチャット'
-ORDER BY id;
+AND overview LIKE '%ダイレクトチャット'
+ORDER BY id ASC;
