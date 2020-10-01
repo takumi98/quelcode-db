570a74e0026463b34c55e3553e5f9c0e9ccad6b3
takumi <takumik9823@gmail.com>
2020-10-01 18:40:24 +0900

    db013

--- db/db-challenge3/db-013/db-013.sql
+++ db/db-challenge3/db-013/db-013.sql
@@ -1,8 +1,11 @@
 BEGIN;
 UPDATE users
     SET is_deleted = true
-WHERE (work_phone_number IS NULL
-    AND mobile_phone_number IS NULL)
-    OR (work_phone_number = ''
-    AND mobile_phone_number = '');
+WHERE (work_phone_number IS NULL AND mobile_phone_number IS NULL)
+    OR 
+    (work_phone_number = '' AND mobile_phone_number = '')
+    OR 
+    (work_phone_number = '' AND mobile_phone_number IS NULL)
+    OR 
+    (work_phone_number IS NULL AND mobile_phone_number = '');
 COMMIT;
